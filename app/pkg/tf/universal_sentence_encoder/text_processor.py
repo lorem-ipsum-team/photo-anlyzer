29a835631485122db7306dbfa0b8c711d00da6cb
Antttezy <antkumachev@gmail.com>
2025-11-28 01:00:15 +0300

    .

--- app/pkg/tf/universal_sentence_encoder/text_processor.py
+++ app/pkg/tf/universal_sentence_encoder/text_processor.py
@@ -1,13 +1,16 @@
 import tensorflow_hub as hub
 import numpy as np
 import tensorflow_text
+import fasttext
 from app.internal.config import USE_MODEL_URL
 
 
 class TextProcessor:
     def __init__(self):
+        self.model = fasttext.load_model('cc.ru.300.bin')
         self.embed = hub.load(USE_MODEL_URL)
 
     def process_text(self, sentence: str) -> list:
-        embedding = self.embed(sentence)['outputs'][0]
-        return np.asarray(embedding).tolist()
+        sentence = sentence.replace('\n', ' ')
+        embedding = self.model.get_sentence_vector(sentence)
+        return embedding.tolist()
